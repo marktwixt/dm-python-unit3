a76dedc7eb1326327f2f96c27a3383fdacc96812
Walt Ostrander <waltostrander@gmail.com>
2023-04-10 19:20:46 -0500

    completed part 3

--- server.py
+++ server.py
@@ -1,49 +1,57 @@
-from flask import Flask, render_template, redirect, flash, request
-import jinja2
-import melons
-from melons import get_all, get_by_id  # Import functions from melons.py
+from flask import Flask, render_template, flash, session, redirect, url_for
+from melons import Melon, get_all, get_by_id
 
 app = Flask(__name__)
-app.jinja_env.undefined = jinja2.StrictUndefined  # for debugging purposes
+app.secret_key = 'dev'
 
-### Flask Routes ###
 @app.route("/")
 def homepage():
-   return render_template("base.html")
+    return render_template("base.html")
 
-@app.route("/melons")
-def all_melons():
-    """Return a page listing all the melons available for purchase."""
+@app.route('/melons')
+def list_melons():
+    all_melons = get_all()
+    return render_template('all_melons.html', melons=all_melons, int=int)
 
-    melon_list = get_all()  # Call the get_all function to retrieve the list of melons
-    return render_template("all_melons.html", melon_list=melon_list)
-
-@app.route("/melon/<melon_id>")
-def melon_details(melon_id):
-   """Return a page showing all info about a melon. Also, provide a button to buy that melon."""
-
-   melon = get_by_id(melon_id)
-   return render_template("melon_details.html", melon=melon)
-
-@app.route("/add_to_cart/<melon_id>")
-def add_to_cart(melon_id):
-   """Add a melon to the shopping cart."""
-
-   return f"{melon_id} added to cart"
+@app.route("/melons/<melon_id>")
+def show_melon(melon_id):
+    melon = get_by_id(melon_id)
+    return render_template("melon_details.html", melon=melon)
 
 @app.route("/cart")
 def show_shopping_cart():
-   """Display contents of shopping cart."""
+    order_total = 0
+    cart_melons = []
 
-   # Need a function that returns cart items and total price
-   # cart_items = get_cart_items()
-   # total_price = get_total_price()
+    cart = session.get("cart", {})
 
-   # If not, you can use dummy data for now:
-   cart_items = []
-   total_price = 0.0
+    for melon_id, qty in cart.items():
+        melon = get_by_id(melon_id)
+        total_cost = melon.price * qty
+        order_total += total_cost
 
-   return render_template("cart.html", cart_items=cart_items, total_price=total_price)
+        melon.quantity = qty
+        melon.total_cost = total_cost
+        cart_melons.append(melon)
+
+    return render_template("cart.html", cart_melons=cart_melons, order_total=order_total)
+
+@app.route("/add-to-cart/<melon_id>")
+def add_to_cart(melon_id):
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    session['cart'][str(melon_id)] = session['cart'].get(str(melon_id), 0) + 1
+    session.modified = True
+
+    flash(f"Melon {melon_id} successfully added to cart.")
+    return redirect(url_for("show_shopping_cart"))
+
+@app.route("/empty-cart")
+def empty_cart():
+    session['cart'] = {}
+    session.modified = True
+    return redirect(url_for("show_shopping_cart"))
 
 if __name__ == "__main__":
    app.env = "development"
